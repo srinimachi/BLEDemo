3cb3214e1af95cebfb192af606e8cb86af8233a1
srinimachi <srinivasan.raman@thule.com>
2024-01-25 09:06:57 +0100

    widget not reloading in func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?)

--- BLEDemo/ContentView.swift
+++ BLEDemo/ContentView.swift
@@ -9,12 +9,13 @@ import SwiftUI
 import CoreBluetooth
 import WidgetKit
 
-class BluetoothViewModel: NSObject, ObservableObject, CBCentralManagerDelegate {
+class BluetoothViewModel: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeripheralDelegate {
     @Published var isConnected = false
 
     private var centralManager: CBCentralManager!
     private var peripheral: CBPeripheral?
     private let targetDeviceUUID = UUID(uuidString:"049C4A37-3941-86AA-9008-EF95B8985AC2")
+    let SOME_BLE_CHARACTERISTIC = CBUUID(string: "0000000b-c898-4ba4-9488-70d046f56724")
 
     override init() {
         super.init()
@@ -30,11 +31,14 @@ class BluetoothViewModel: NSObject, ObservableObject, CBCentralManagerDelegate {
     func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String: Any], rssi RSSI: NSNumber) {
         if peripheral.identifier == targetDeviceUUID {
             self.peripheral = peripheral
+            self.peripheral?.delegate = self
             central.connect(peripheral, options: nil)
         }
     }
 
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        self.peripheral?.discoverServices(nil) //can provide array of specific services
+        self.peripheral?.delegate = self
         isConnected = true
         print("reload widget to update time of connect")
         WidgetCenter.shared.reloadAllTimelines()
@@ -44,6 +48,34 @@ class BluetoothViewModel: NSObject, ObservableObject, CBCentralManagerDelegate {
         isConnected = false
         print("reload widget to update time of disconnect")
         WidgetCenter.shared.reloadAllTimelines()
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        for charac in service.characteristics!{
+            if charac.uuid == SOME_BLE_CHARACTERISTIC{
+                peripheral.setNotifyValue(true, for: charac)
+                peripheral.readValue(for: charac)
+            }
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        if let services = peripheral.services{
+            for service in services{
+                peripheral.discoverCharacteristics(nil, for: service)
+            }
+        }
+    }
+    
+    
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        if characteristic.uuid == SOME_BLE_CHARACTERISTIC
+        {
+            
+            //HERE the widget is not reloading in background
+            print("reload widget to update time of characterstic")
+            WidgetCenter.shared.reloadAllTimelines()
+        }
     }
 }
 
